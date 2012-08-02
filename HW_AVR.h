0f1fa5d4a5c9045a1d7c6dad6e206fb64f3222b6
Tony Ivanov <telamohn@gmail.com>
2012-08-02 16:28:24 +0200

    Removed some compatibility code to save 1.5kB space

--- HW_AVR.h
+++ HW_AVR.h
@@ -1,85 +1,6 @@
 // *** Hardwarespecific functions ***
 void UTFT::LCD_Writ_Bus(char VH,char VL, byte mode)
 {   
-	switch (mode)
-	{
-	case 1:
-		if (display_serial_mode==SERIAL_4PIN)
-		{
-		if (VH==1)
-			sbi(P_SDA, B_SDA);
-		else
-			cbi(P_SDA, B_SDA);
-		pulse_low(P_SCL, B_SCL);
-		}
-		else
-		{
-		if (VH==1)
-			sbi(P_RS, B_RS);
-		else
-			cbi(P_RS, B_RS);
-		}
-
-		if (VL & 0x80)
-			sbi(P_SDA, B_SDA);
-		else
-			cbi(P_SDA, B_SDA);
-		pulse_low(P_SCL, B_SCL);
-		if (VL & 0x40)
-			sbi(P_SDA, B_SDA);
-		else
-			cbi(P_SDA, B_SDA);
-		pulse_low(P_SCL, B_SCL);
-		if (VL & 0x20)
-			sbi(P_SDA, B_SDA);
-		else
-			cbi(P_SDA, B_SDA);
-		pulse_low(P_SCL, B_SCL);
-		if (VL & 0x10)
-			sbi(P_SDA, B_SDA);
-		else
-			cbi(P_SDA, B_SDA);
-		pulse_low(P_SCL, B_SCL);
-		if (VL & 0x08)
-			sbi(P_SDA, B_SDA);
-		else
-			cbi(P_SDA, B_SDA);
-		pulse_low(P_SCL, B_SCL);
-		if (VL & 0x04)
-			sbi(P_SDA, B_SDA);
-		else
-			cbi(P_SDA, B_SDA);
-		pulse_low(P_SCL, B_SCL);
-		if (VL & 0x02)
-			sbi(P_SDA, B_SDA);
-		else
-			cbi(P_SDA, B_SDA);
-		pulse_low(P_SCL, B_SCL);
-		if (VL & 0x01)
-			sbi(P_SDA, B_SDA);
-		else
-			cbi(P_SDA, B_SDA);
-		pulse_low(P_SCL, B_SCL);
-		break;
-	case 8:
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-		PORTA = VH;
-		pulse_low(P_WR, B_WR);
-		PORTA = VL;
-		pulse_low(P_WR, B_WR);
-#elif defined(__AVR_ATmega32U4__)
-	/*
-		Pin TFT   Leo   Uno
-		-------------------
-		D0  DB8   PD2   PD0
-		D1  DB9   PD3   PD1
-		D2  DB10  PD1   PD2
-		D3  DB11  PD0   PD3
-		D4  DB12  PD4   PD4
-		D5  DB13  PC6   PD5
-		D6  DB14  PD7   PD6
-		D7  DB15  PE6   PD7
-	*/
 	if(VH & 0x01)
 		PORTD |= (1<<2);
 	else
@@ -146,27 +67,7 @@ void UTFT::LCD_Writ_Bus(char VH,char VL, byte mode)
 	else
 		PORTE &= ~(1<<6);	
 	pulse_low(P_WR, B_WR);
-#else
-		PORTD = VH;
-		pulse_low(P_WR, B_WR);
-		PORTD = VL;
-		pulse_low(P_WR, B_WR);
 
-#endif
-		break;
-	case 16:
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-		PORTA = VH;
-		PORTC = VL;
-#else
-		PORTD = VH;
-		cport(PORTC, 0xFC);
-		sport(PORTC, (VL>>6) & 0x03);
-		PORTB =  VL & 0x3F;
-#endif
-		pulse_low(P_WR, B_WR);
-		break;
-	}
 }
 
 void UTFT::_set_direction_registers(byte mode)
